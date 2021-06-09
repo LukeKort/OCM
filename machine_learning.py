c0599e510944a3d198be2ef35e398bcddc4a5010
Lucas Kort <lucas.kort@outlook.com>
2021-06-09 14:51:28 -0300

    Update 2

--- machine_learning.py
+++ machine_learning.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 
 def weibull_layer(x):
     """
@@ -30,20 +31,22 @@ def weibull_layer(x):
     
     # Add one dimension to make the right shape
     theta = tf.expand_dims(theta, -1)
-    gamma = tf.expand_dims(theta, -1)
+    gamma = tf.expand_dims(gamma, -1)
         
     # Apply a softplus to make positive
     theta = tf.keras.activations.softplus(theta)
-    gamma = tf.keras.activations.sigmoid(gamma)
+    gamma = tf.keras.activations.softplus(gamma)
+
+    #print('theta',theta.get_shape(),'\ngama',gamma.get_shape()) #teste de qualidade apenas
  
     # Join back together again
     out_tensor = tf.concat((theta, gamma), axis=num_dims-1)
 
     return out_tensor
 
-def negative_weibull_loss(y_true, y_pred):
+def weibull_loss(y_true, y_pred):
     """
-    Negative binomial loss function.
+    Negative log likelihood weibull loss function.
     Assumes tensorflow backend.
     
     Parameters
@@ -66,14 +69,22 @@ def negative_weibull_loss(y_true, y_pred):
     theta = tf.expand_dims(theta, -1)
     gamma = tf.expand_dims(gamma, -1)
     
+    log_sum = 0
+    t_gamma_sum = 0
+
+    size = y_true.get_shape()[-1] #tamanho do vetor
+
+    for i in range(size):
+        log_sum += tf.math.log(y_true[i])
+        t_gamma_sum += y_true**gamma
+
     # Calculate the negative log likelihood
     nll = (
-        tf.math.lgamma(theta) 
-        + tf.math.lgamma(y_true + 1)
-        - tf.math.lgamma(theta + y_true)
-        - theta * tf.math.log(gamma)
-        - y_true * tf.math.log(1 - gamma)
-    )                  
+        - tf.math.log(gamma) 
+        + tf.math.log(theta)
+        - (gamma - 1)*log_sum
+        + (1/theta)*t_gamma_sum*tf.math.log(math.e)
+    )
 
     return nll
 
@@ -81,9 +92,12 @@ from tensorflow.keras.layers import Input, Dense, Lambda
 from tensorflow.keras.models import Model
 
 # Define inputs with predefined shape
-inputs = Input(shape=input_shape)
+inputs = Input(shape=(5,))
 
 # Build network with some predefined architecture
+Layer1 = Dense(units=16)
+Layer2 = Dense(units=16)
+
 output1 = Layer1(inputs)
 output2 = Layer2(output1)
 
@@ -97,6 +111,35 @@ model = Model(inputs=inputs, outputs=distribution_outputs)
 from tensorflow.keras.optimizers import Adam
 
 opt = Adam()
-model.compile(loss = negative_weibull_loss, optimizer = opt)
+model.compile(loss = weibull_loss, optimizer = opt)
 
-history = model.fit(train_X, train_Y, epochs = num_epochs, validation_data = [val_X, val_Y])
+import pandas as pd
+import numpy as np
+
+time = pd.read_excel(
+    'data.xlsx',
+    sheet_name= 'Tempos',
+    index_col='Index',
+) #import data from xlsx file
+header = []
+vector_n,vector_size = time.shape #tamanho da última dimensão
+
+for i in range(vector_size): #cabeçalho para os tempos
+        header.append('T' + str(i+1))
+
+f_t = pd.read_excel(
+    'data.xlsx',
+    sheet_name= 'Dados simulados',
+    index_col='Index',
+) #import data from xlsx file
+
+train_x = time.loc[range(vector_n),header].values.tolist()
+train_y = f_t.loc[range(vector_n),range(vector_size)].values.tolist()
+
+history = model.fit(train_x, train_y, epochs = 10000) # validation_data = [val_X, val_Y])
+
+test_t = train_x = time.loc[range(590,vector_n),header].values.tolist()
+
+pred_params = model.predict(test_t)
+
+print(pred_params)
